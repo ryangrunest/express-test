2c390849ac10bfb89493361904378af228d413dc
Ryan Grunest <ryangrunest45@gmail.com>
2019-04-24 09:48:40 -0700

    created very html routes

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+// variables used to initialize express
 const express = require('express');
+const app = express();
+
+// creating static port and hostname variables to start the server
+const port = process.env.PORT || 8080;
+const hostname = 'Localhost';
+
+
+// HTML ROUTES
+app.get('/', (req, res) => {
+  res.send('Hi There!');
+});
+app.get('/bye', (req, res) =>  {
+  res.send('Goodbye!');
+});
+app.get('/dog', (req,res) => {
+  res.send('MEOW!');
+});
+
+
+// tell express  to listen for requests (Start Server)
+app.listen(port, hostname, () => { console.log('server has started') });
